196ef2cce133b0b21cb99d7eba3b7eee738be46c
mabus <m.buscaroli@outlook.com>
2022-01-08 11:09:41 +0000

    setting up for live-reload

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const { get } = require('express/lib/response')
+
+// CONSTANTS
+const app = express()
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// SETUP
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirPath))
+
+// ROUTES
+
+app.get('', (request, response) => {
+     response.render('index', {
+         title: "A Simple Site with APIs"
+     })
+})
+
+
+
+
+app.get('*', (request, response) => {
+     response.send('404 NOT FOUND.')
+})
+
+
+// LAUNCHING SERVER
+app.listen(3000, () => {
+    console.log(`Server is Listening on 3000`)
+})
